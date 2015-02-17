2b3fab74419a844a3453f8f2025ceea860a00eb5
Parfunc Account <parfunc@googlegroups.com>
2015-02-17 13:55:56 -0500

    update

--- Setup.sh
+++ Setup.sh
@@ -2,5 +2,6 @@
 
 cabal sandbox init 
 
-cabal install ./Accelerate/ ./Accelerate-cuda/ ./Accelerate-examples/ ./Accelerate-io/ ./accelerate-fft/ ./cuda/ ./gloss/gloss/ ./gloss-raster-accelerate/ ./gloss-accelerate/ 
+cabal install ./Accelerate/ ./Accelerate-cuda/ ./Accelerate-examples/ ./Accelerate-io/ ./accelerate-fft/ ./cuda/ ./gloss/gloss/ ./gloss/gloss-raster/ ./gloss/gloss-rendering/ ./gloss-raster-accelerate/ ./gloss-accelerate/ -fMULTI -fcuda
+
 
