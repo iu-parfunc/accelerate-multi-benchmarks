cb2ff7d0d6baa0c9ff415aaff7628026caa48446
Joel Svensson <svenssonjoel@yahoo.se>
2015-02-16 23:27:26 -0500

    added Setup.sh

---
+++ Setup.sh
@@ -0,0 +1,6 @@
+
+
+cabal sandbox init 
+
+cabal install ./Accelerate/ ./Accelerate-cuda/ ./Accelerate-examples/ ./Accelerate-io/ ./accelerate-fft/ ./cuda/ ./gloss/gloss/ ./gloss-raster-accelerate/ ./gloss-accelerate/ 
+
