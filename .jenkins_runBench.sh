5c6b8102f6e25e85d89ec306f16d0646a99c89b9
Ryan Newton <rrnewton@gmail.com>
2015-02-18 11:39:23 -0500

    Tweak the benchmarking scripts and get ready to try a Jenkins run

--- .jenkins_runBench.sh
+++ .jenkins_runBench.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+CRITUPLOAD=hsbencher-fusion-upload-criterion-0.3.10
+CSVUPLOAD=hsbencher-fusion-upload-csv-0.3.10
+CATCSV=cat-csv
+
+# The new accelerate-multi-benchmarks table 
+# TABID=1giYspfeb2FPqprb7bEkQo-QnqdvXgXCrcH7jF0Ag
+TABLENAME=accelerate-multi-benchmarks
+
+# Parfunc account, registered app in api console:
+export HSBENCHER_GOOGLE_CLIENTID=905767673358.apps.googleusercontent.com
+export HSBENCHER_GOOGLE_CLIENTSECRET=2a2H57dBggubW1_rqglC7jtK
+
+
+
 echo "Begin running jenkins benchmark script for Accelerate-multidev..."
 set -x
 
 # CONVENTION: The working directory is passed as the first argument.
 CHECKOUT=$1
 shift
+EXTRAARGS=$*
 
 if [ "$CHECKOUT" == "" ]; then
  echo "Replacing CHECKOUT with pwd" 
@@ -13,7 +28,7 @@ if [ "$CHECKOUT" == "" ]; then
 fi
 
 if [ "$JENKINS_GHC" == "" ]; then
-  export JENKINS_GHC=7.6.3
+  export JENKINS_GHC=7.8.3
 fi
 if [ -f "$HOME/continuous_testing_setup/rn_jenkins_scripts/acquire_ghc.sh" ]; then
   source $HOME/continuous_testing_setup/rn_jenkins_scripts/acquire_ghc.sh
@@ -24,17 +39,43 @@ fi
 
 echo "Running benchmarks remotely on server `hostname`"
 
-which cabal
-cabal --version
+export CABAL=cabal-1.20
+
+which $CABAL
+$CABAL --version
 
 which c2hs     || echo ok
 c2hs --version || echo ok
 
 unset GHC
 unset GHC_PKG
-unset CABAL
+
+# If we don't have the Criterion uploader, don't bother trying
+if ! [ -x `which $CRITUPLOAD` ]; then
+    echo "Error: no $CRITUPLOAD found"
+    exit 1
+fi
 
 set -e
+
+cd "$CHECKOUT"
+
+export GIT_DEPTH=`git log --pretty=oneline | wc -l`
+echo "Running at GIT_DEPTH:" $GIT_DEPTH
+
+TAG=`date +'%s'`
+BAKDIR=$HOME/benchdata_bak/$TABLENAME/depth_${GIT_DEPTH}/
+WINDIR=$BAKDIR/uploaded
+FAILDIR=$BAKDIR/failed_upload
+
+mkdir -p $WINDIR
+mkdir -p $FAILDIR
+
+# Criterion regressions
+REGRESSES="--regress=allocated:iters --regress=bytesCopied:iters --regress=cycles:iters \
+--regress=numGcs:iters --regress=mutatorWallSeconds:iters --regress=gcWallSeconds:iters \
+--regress=cpuTime:iters "
+
 
 #sandboxes are created by the Makefile for run_benchmarks.hs
 #this makefile also install Obsidian+hsbencher as a single cabal install line
@@ -52,18 +93,53 @@ set -e
 # NOTES: 
 # On tesla we need to do a module add cuda 
 
+# export TRIALS=1
 
 
-export TRIALS=1
+# Step 1: Build everything
+# ========================================
 
-# Parfunc account, registered app in api console:
-CID=905767673358.apps.googleusercontent.com
-SEC=2a2H57dBggubW1_rqglC7jtK
+./Setup.sh
 
-# The new accelerate-multi-benchmarks table 
-# 1giYspfeb2FPqprb7bEkQo-QnqdvXgXCrcH7jF0Ag
-TABID=1giYspfeb2FPqprb7bEkQo-QnqdvXgXCrcH7jF0Ag
-#NOTE: TABID is not used below!
+# Step 2: Run benchmarks, archive results
+# ========================================
 
-echo "Running Benchmarks"
-./run_benchmarks.exe --keepgoing --trials=$TRIALS --fusion-upload --name=accelerate-multi-benchmarks --clientid=$CID --clientsecret=$SEC $*
+# We don't have a full HSBencher harness here:
+# ./run_benchmarks.exe --keepgoing --trials=$TRIALS --fusion-upload --name=$TABLENAME --clientid=$CID --clientsecret=$SEC $EXTRAARGS
+
+
+BINDIR=`pwd`/.cabal-sandbox/bin
+
+# Accumulator for output names:
+OUTCSVS=
+
+for executable in accelerate-nbody ; do 
+  echo "Running benchmark $executable"
+  REPORT=report_${executable}
+  CRITREPORT=${TAG}_${REPORT}.crit
+  CSVREPORT=${TAG}_${REPORT}.csv
+
+# case $executable
+
+  for arg in 50000 ; do
+      VARIANT=cuda
+      $BINDIR/accelerate-nbody $REGRESSES --$VARIANT -n $arg --benchmark \
+          --output=$CRITREPORT.html --raw=$CRITREPORT  +RTS -T -s
+
+      $CRITUPLOAD --noupload --csv=$CSVREPORT --variant=$VARIANT --threads=1 --args="$arg" $CRITREPORT
+      OUTCSVS+=" $CSVREPORT"
+    done  
+done
+
+echo "Finally: attempt an upload"
+ALLREPS=${TAG}_ALLDATA.csv
+$CATCSV $OUTCSVS > $BAKDIR/$ALLREPS
+
+# NOTE: could aggressively retry this, since we're alreday done with the benchmarks:
+$CSVUPLOAD $BAKDIR/$ALLREPS --fusion-upload --name=$TABLENAME || FAILED=1
+
+if [ "$FAILED" == 1 ]; then
+   mv $BAKDIR/$ALLREPS $FAILDIR/
+else
+   mv $BAKDIR/$ALLREPS $WINDIR/
+fi
--- Setup.sh
+++ Setup.sh
@@ -1,7 +1,19 @@
+#!/bin/bash
 
+# Compile all the packages
 
-cabal sandbox init 
+set -xe
 
-cabal install ./Accelerate/ ./Accelerate-cuda/ ./Accelerate-examples/ ./Accelerate-io/ ./accelerate-fft/ ./cuda/ ./gloss/gloss/ ./gloss/gloss-raster/ ./gloss/gloss-rendering/ ./gloss-raster-accelerate/ ./gloss-accelerate/ -fMULTI -fcuda
+if [ "$CABAL" == "" ]; then 
+  CABAL=cabal-1.20
+fi
 
+$CABAL sandbox init 
 
+if ! [ -e ./cuda/configure ]; then
+  cp ./aux/configure ./cuda/
+fi
+
+PKGS="./Accelerate/ ./Accelerate-cuda/ ./Accelerate-examples/ ./Accelerate-io/ ./accelerate-fft/ ./cuda/ ./gloss/gloss/ ./gloss/gloss-raster/ ./gloss/gloss-rendering/ ./gloss-raster-accelerate/ ./gloss-accelerate/ "
+
+$CABAL install -fMULTI -fcuda $PKGS -j --ghc-option=-j3
