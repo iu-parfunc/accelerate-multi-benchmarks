3a1a5db70837ba7d530574d1c97751c767c9c162
Joel Svensson <svenssonjoel@yahoo.se>
2015-02-19 17:50:38 -0500

    update

--- .jenkins_runBench.sh
+++ .jenkins_runBench.sh
@@ -137,7 +137,7 @@ function go {
   OUTCSVS+=" ${CSVREPORT}_${VARIANT}_${arg}.csv"
 }
 
-for executable in megapar accelerate-nbody accelerate-mandelbrot ; do 
+for executable in accelerate-nbody accelerate-mandelbrot megapar; do 
   echo "Running benchmark $executable"
   REPORT=report_${executable}
   CRITREPORT=${TAG}_${REPORT}
