addf90c198df597de415c9bb225d1bf5d1fbcea2
Joel Svensson <svenssonjoel@yahoo.se>
2015-02-19 20:09:47 -0500

    reenable cuda megapar

--- .jenkins_runBench.sh
+++ .jenkins_runBench.sh
@@ -170,12 +170,12 @@ for executable in megapar accelerate-nbody accelerate-mandelbrot ; do
      megapar) 
        for arg in 1 2 3 4; do 
 	   VARIANT=$variant 
-	   if [ $VARIANT != cuda ]; then 
+	   #if [ $VARIANT != cuda ]; then 
 	     $BINDIR/megapar --$VARIANT  -n $arg --benchmark \
                  --output=${CRITREPORT}_${VARIANT}_${arg}.html --raw=${CRITREPORT}_${VARIANT}_${arg}.crit  +RTS -T -s 
 	     $CRITUPLOAD --noupload --csv=${CSVREPORT}_${VARIANT}_${arg}.csv --variant=$VARIANT --threads=1 --args="$arg" ${CRITREPORT}_${VARIANT}_${arg}.crit
 	     OUTCSVS+=" ${CSVREPORT}_${VARIANT}_${arg}.csv"
-	   fi 
+	   #fi 
        done
        ;;
     esac
