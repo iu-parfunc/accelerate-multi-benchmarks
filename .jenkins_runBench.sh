69cf8534789e04613cf6308efafd969ce2b32d96
Mike Vollmer <mike@recurial.com>
2015-02-27 11:02:15 -0500

    go bigger on matrix multiply

--- .jenkins_runBench.sh
+++ .jenkins_runBench.sh
@@ -184,7 +184,7 @@ for executable in $EXTRAARGS; do
        done
        ;;
      accelerate-mmult) 
-       for arg in 100 200 300 400 500 600 700 800; do	       
+       for arg in 100 200 300 400 500 600 700 800 900 1000; do	       
 	   ARGUMENTS="--$variant  -n $arg --benchmark --output=${CRITREPORT}_${variant}_${arg}.html --raw=${CRITREPORT}_${variant}_${arg}.crit --time-limit=60 +RTS -T -s"
 	   go 0;
        done
@@ -280,7 +280,7 @@ for executable in $EXTRAARGS; do
       done
       ;;
     accelerate-mmult) 
-      for arg in 100 200 300 400 500 600 700 800; do
+      for arg in 100 200 300 400 500 600 700 800 900 1000; do
 	  ARGUMENTS="--multi -n $arg --benchmark --output=${CRITREPORT}_${VARIANT}_${arg}.html --raw=${CRITREPORT}_${VARIANT}_${arg}.crit --time-limit=60 +RTS -T -s"
 	  go 1 0;
       done
@@ -333,7 +333,7 @@ for executable in $EXTRAARGS; do
       done
       ;;
     accelerate-mmult) 
-      for arg in 100 200 300 400 500 600 700 800; do
+      for arg in 100 200 300 400 500 600 700 800 900 1000; do
 	  ARGUMENTS="--multi -n $arg --benchmark --output=${CRITREPORT}_${VARIANT}_${arg}.html --raw=${CRITREPORT}_${VARIANT}_${arg}.crit --time-limit=60  +RTS -T -s"
 	  go 1 '0 1';
       done
@@ -382,7 +382,7 @@ for executable in $EXTRAARGS; do
       done
       ;;
     accelerate-mmult) 
-      for arg in 100 200 300 400 500 600 700 800; do
+      for arg in 100 200 300 400 500 600 700 800 900 1000; do
 	  ARGUMENTS="--multi -n $arg --benchmark --output=${CRITREPORT}_${VARIANT}_${arg}.html --raw=${CRITREPORT}_${VARIANT}_${arg}.crit --time-limit=60 +RTS -T -s"
 	  go 0 0;
       done
@@ -423,7 +423,7 @@ for executable in $EXTRAARGS; do
       done
       ;;
     accelerate-mmult) 
-      for arg in 100 200 300 400 500 600 700 800; do
+      for arg in 100 200 300 400 500 600 700 800 900 1000; do
 	  ARGUMENTS="--multi -n $arg --benchmark --output=${CRITREPORT}_${VARIANT}_${arg}.html --raw=${CRITREPORT}_${VARIANT}_${arg}.crit --time-limit=60 +RTS -T -s"
 	  go 0 '0 1';
       done
