1a30c50316de5dc8200bab07841eb632757ae9cb
Mike Vollmer <mike@recurial.com>
2015-02-27 22:13:10 -0500

    Add bigger number to nbody

--- .jenkins_runBench.sh
+++ .jenkins_runBench.sh
@@ -172,7 +172,7 @@ for executable in $EXTRAARGS; do
    VARIANT=$variant 
    case $executable in 
      accelerate-nbody)  	   
-       for arg in 50000 60000 70000 80000 90000 100000 110000; do
+       for arg in 50000 60000 70000 80000 90000 100000 110000 300000; do
 	  ARGUMENTS="--$variant -n $arg --benchmark --output=${CRITREPORT}_${variant}_${arg}.html --raw=${CRITREPORT}_${variant}_${arg}.crit +RTS -T -s"
 	  go 0; 
        done  
@@ -274,7 +274,7 @@ for executable in $EXTRAARGS; do
   VARIANT=multi_one_device_fissed
   case $executable in 
     accelerate-nbody) 
-      for arg in 50000 60000 70000 80000 90000 100000 110000; do 
+      for arg in 50000 60000 70000 80000 90000 100000 110000 300000; do 
 	ARGUMENTS="--multi -n $arg --benchmark --output=${CRITREPORT}_${VARIANT}_${arg}.html --raw=${CRITREPORT}_${VARIANT}_${arg}.crit +RTS -T -s"
 	go 1 0;
       done  
@@ -334,7 +334,7 @@ for executable in $EXTRAARGS; do
   VARIANT=multi_two_device_fissed
   case $executable in 
     accelerate-nbody) 
-      for arg in 50000 60000 70000 80000 90000 100000 110000; do 
+      for arg in 50000 60000 70000 80000 90000 100000 110000 300000; do 
         ARGUMENTS="--multi -n $arg --benchmark --output=${CRITREPORT}_${VARIANT}_${arg}.html --raw=${CRITREPORT}_${VARIANT}_${arg}.crit +RTS -T -s"
         go 1 '0 1'; 
       done  
@@ -389,7 +389,7 @@ for executable in $EXTRAARGS; do
   VARIANT=multi_one_device_unfissed
   case $executable in 
     accelerate-nbody) 
-      for arg in 50000 60000 70000 80000 90000 100000 110000; do 
+      for arg in 50000 60000 70000 80000 90000 100000 110000 300000; do 
 	ARGUMENTS="--multi -n $arg --benchmark --output=${CRITREPORT}_${VARIANT}_${arg}.html --raw=${CRITREPORT}_${VARIANT}_${arg}.crit +RTS -T -s"
         go 0 0;
       done  
@@ -438,7 +438,7 @@ for executable in $EXTRAARGS; do
   VARIANT=multi_two_device_unfissed
   case $executable in 
     accelerate-nbody) 
-      for arg in 50000 60000 70000 80000 90000 100000 110000; do 
+      for arg in 50000 60000 70000 80000 90000 100000 110000 300000; do 
         ARGUMENTS="--multi -n $arg --benchmark --output=${CRITREPORT}_${VARIANT}_${arg}.html --raw=${CRITREPORT}_${VARIANT}_${arg}.crit +RTS -T -s"
 	go 0 '0 1';
       done  
